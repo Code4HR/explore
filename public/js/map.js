4cad7b01a1af0a46d20e10d92dc6eb91cbaeed95
alicia.sedarski <alicia.sedarski@dominionenterprises.com>
2016-02-27 14:53:51 -0500

    Cleaning up code

--- public/js/map.js
+++ public/js/map.js
@@ -34,7 +34,7 @@ function initAutocomplete() {
             styles: styleArray
         };
         map = new google.maps.Map(document.getElementById('map'),
-                                  mapOptions);
+            mapOptions);
         var pointArray = new google.maps.MVCArray(Datas);
         crimeheatmap = new google.maps.visualization.HeatmapLayer({
             data: pointArray,
@@ -52,7 +52,7 @@ function initAutocomplete() {
     }
     google.maps.event.addDomListener(window, 'load', initialize);
 
-    $('#crimeSlider').change(function(){
+    $('#crimeSlider').change(function () {
         var crimeHeatMapData = [];
         var data = {};
         data.slidervalue = $(this).text();
@@ -61,15 +61,15 @@ function initAutocomplete() {
             url: '/crime',
             data: data,
             dataType: 'json',
-            success: function(crimeData) {
+            success: function (crimeData) {
                 for (var elem = 0, max = crimeData.length; elem < max; elem++) {
-                    crimeHeatMapData.push({location: new google.maps.LatLng(crimeData[elem].latitude, crimeData[elem].longitude), weight: crimeData[elem].severity});
+                    crimeHeatMapData.push({ location: new google.maps.LatLng(crimeData[elem].latitude, crimeData[elem].longitude), weight: crimeData[elem].severity });
                 }
                 crimeheatmap.set('data', crimeHeatMapData);
             }
         });
     });
-    $('#colSlider').change(function(){
+    $('#colSlider').change(function () {
         var colHeatMapData = [];
         var data = {};
         data.slidervalue = $(this).text();
@@ -78,15 +78,15 @@ function initAutocomplete() {
             url: '/col',
             data: data,
             dataType: 'json',
-            success: function(colData) {
+            success: function (colData) {
                 for (var elem = 0, max = colData.length; elem < max; elem++) {
-                    colHeatMapData.push({location: new google.maps.LatLng(colData[elem].lat, colData[elem].lon), weight: colData[elem].weight});
+                    colHeatMapData.push({ location: new google.maps.LatLng(colData[elem].lat, colData[elem].lon), weight: colData[elem].weight });
                 }
                 colheatmap.set('data', colHeatMapData);
             }
         });
     });
-    $('#foodSanitationSlider').change(function(){
+    $('#foodSanitationSlider').change(function () {
         var newHeatMapData = [];
         var data = {};
         data.slidervalue = $(this).text();
@@ -96,11 +96,11 @@ function initAutocomplete() {
             url: '/food/sanitation',
             data: data,
             dataType: 'json',
-            success: function(foodSanitationData) {
+            success: function (foodSanitationData) {
                 for (var elem = 0, max = foodSanitationData.length; elem < max; elem++) {
-                    if (foodSanitationData[elem].latitude !== null && foodSanitationData[elem].longitude !== null){
+                    if (foodSanitationData[elem].latitude !== null && foodSanitationData[elem].longitude !== null) {
                         foodSanitationMarker = new google.maps.Marker({
-                            position: {lat: foodSanitationData[elem].latitude, lng: foodSanitationData[elem].longitude},
+                            position: { lat: foodSanitationData[elem].latitude, lng: foodSanitationData[elem].longitude },
                             title: foodSanitationData[elem].name,
                             map: map,
                             icon: foodSanitationImage
@@ -110,66 +110,4 @@ function initAutocomplete() {
             }
         });
     });
-
-
-
-    //    // Create the search box and link it to the UI element.
-    //    var input = document.getElementById('pac-input');
-    //    var searchBox = new google.maps.places.SearchBox(input);
-    //    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    //
-    //    // Bias the SearchBox results towards current map's viewport.
-    //    map.addListener('bounds_changed', function() {
-    //        searchBox.setBounds(map.getBounds());
-    //    });
-    //
-    //
-    //    var markers = [];
-    //    // [START region_getplaces]
-    //    // Listen for the event fired when the user selects a prediction and retrieve
-    //    // more details for that place.
-    //    searchBox.addListener('places_changed', function() {
-    //        var places = searchBox.getPlaces();
-    //
-    //        if (places.length == 0) {
-    //            return;
-    //        }
-    //
-    //        // Clear out the old markers.
-    //        markers.forEach(function(marker) {
-    //            marker.setMap(null);
-    //        });
-    //        markers = [];
-    //
-    //        // For each place, get the icon, name and location.
-    //        var bounds = new google.maps.LatLngBounds();
-    //        places.forEach(function(place) {
-    //            var icon = {
-    //                url: place.icon,
-    //                size: new google.maps.Size(71, 71),
-    //                origin: new google.maps.Point(0, 0),
-    //                anchor: new google.maps.Point(17, 34),
-    //                scaledSize: new google.maps.Size(25, 25)
-    //            };
-    //
-    //            // Create a marker for each place.
-    //            markers.push(new google.maps.Marker({
-    //                map: map,
-    //                icon: icon,
-    //                title: place.name,
-    //                position: place.geometry.location
-    //            }));
-    //
-    //            if (place.geometry.viewport) {
-    //                // Only geocodes have viewport.
-    //                bounds.union(place.geometry.viewport);
-    //            } else {
-    //                bounds.extend(place.geometry.location);
-    //            }
-    //        });
-    //        map.fitBounds(bounds);
-    //    });
-    //    // [END region_getplaces]
-
-
 }
